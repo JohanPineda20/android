f7a2ff68ea6125fc01288d209e63f07c7ecbc50a
JohanPineda20 <johanmanuelph@ufps.edu.co>
2022-10-31 13:54:20 -0500

    mostrando tiendas al consumir la base de datos de firebase

---
+++ app/src/main/java/com/example/myfirstmobileapp/modelo/Tienda.kt
@@ -0,0 +1,3 @@
+package com.example.myfirstmobileapp.modelo
+
+data class Tienda(val id: String, val nombre: String, val descripcion: String, val longitud: String, val latitud: String, val telefono: String, val imagen: String)
--- app/src/main/java/com/example/myfirstmobileapp/vista/Tienda.kt
+++ app/src/main/java/com/example/myfirstmobileapp/vista/Tienda.kt
@@ -1,11 +1,19 @@
 package com.example.myfirstmobileapp.vista
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myfirstmobileapp.R
+import com.example.myfirstmobileapp.controlador.TiendaAdapter
+import com.example.myfirstmobileapp.modelo.Tienda
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +22,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Tienda.newInstance] factory method to
+ * Use the [tienda.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Tienda : Fragment() {
+class tienda : Fragment() {
+    lateinit var contenedorTienda: RecyclerView
+    lateinit var tiendaAdapter: TiendaAdapter
+    lateinit var agregarTienda: FloatingActionButton
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,8 +47,74 @@ class Tienda : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tienda, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_tienda, container, false)
+        contenedorTienda = view.findViewById(R.id.contenedor_tienda)
+        val linearLayout = LinearLayoutManager(context)
+        linearLayout.orientation = LinearLayoutManager.VERTICAL
+        contenedorTienda.layoutManager=linearLayout
+        tiendaAdapter = TiendaAdapter(context,cargarDatos(), R.id.card)
+        contenedorTienda.adapter=tiendaAdapter
+        agregarTienda= view.findViewById(R.id.agregar_tienda_foa)
+        agregarTienda.setOnClickListener(){
+            addTienda()
+        }
+        return view
     }
+
+    private fun addTienda(){
+        val intent = Intent(context, RegistrarTiendaActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun cargarDatos(): ArrayList<Tienda> {
+        val tiendas: ArrayList<Tienda> = java.util.ArrayList()
+        tiendas.add (Tienda("13a",
+                "Don Jose",
+                "Viveres",
+                "",
+                "",
+                "",
+                "https://thefoodtech.com/wp-content/uploads/2021/03/tiendas-de-autoservicio.jpg"))
+        tiendas.add (Tienda("13a",
+            "Doña Naria",
+            "Viveres",
+            "",
+            "",
+            "",
+            "https://thefoodtech.com/wp-content/uploads/2021/03/tiendas-de-autoservicio.jpg"))
+        return tiendas
+    }
+
+    fun cargarDatosFireBase(): ArrayList<Tienda> {
+        val tiendas= ArrayList<Tienda>()
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance();
+        val myRef: DatabaseReference = database.reference
+        // Read from the database
+        myRef.child("tiendas").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                if (snapshot.exists()){
+                    tiendas.clear()
+                    for(data in snapshot.children){
+                        val tienda=data.getValue(Tienda::class.java)
+                        tiendas.add(tienda as Tienda)
+                        tiendaAdapter.notifyDataSetChanged()
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("Tienda", "Failed to read value.", error.toException())
+            }
+
+        })
+        return tiendas;
+    }
+
 
     companion object {
         /**
@@ -50,7 +128,7 @@ class Tienda : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Tienda().apply {
+            tienda().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
